bf40595cec90f5b5f66b265e6a386376fc001e52
Marius Butuc <marius.butuc@gmail.com>
2016-11-08 22:30:44 -0500

    Remember how to run the code in that source file?

---
+++ intro/hello.exs
@@ -0,0 +1,13 @@
+# $ elixir hello.exs
+#
+# OR…
+#
+# $ iex
+# iex> c "hello.exs"
+#
+# OR…
+#
+# iex> import_file "hello.exs"
+# bonus: local variables set in the file are available in the iex session
+
+IO.puts "Hello, World!"
